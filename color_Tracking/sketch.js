ffc6553ef7ece7ee0cf89e5a7427866b015d899b
diogoboinas <diogoboinas@gmail.com>
2020-11-10 17:01:27 +0000

    capture.style('opacity',0.5)

--- color_Tracking/sketch.js
+++ color_Tracking/sketch.js
@@ -4,22 +4,23 @@ var trackingData;
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
-
   capture = createCapture(VIDEO); //capture the webcam
-  capture.position(0,0) //move the capture to the top left
-  capture.style('opacity',0.5)// use this to hide the capture later on (change to 0 to hide)...
-  capture.id("myVideo"); //give the capture an ID so we can use it in the tracker below.
+  //capture.position(0,0) //move the capture to the top left
+  //capture.style('opacity',0.5)// use this to hide the capture later on (change to 0 to hide)...
+  //capture.id("myVideo"); //give the capture an ID so we can use it in the tracker below.
+  capture.id("cap")
+  capture.hide()
+  
+  colors = new tracking.ColorTracker(['magenta']);
+  tracker("#cap",colors)
+}
 
-  // colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
-    colors = new tracking.ColorTracker(['magenta']);
-
-  tracking.track('#myVideo', colors); // start the tracking of the colors above on the camera in p5
-
+function tracker(id ,colors) {
+  tracking.track(id, colors); // start the tracking of the colors above on the camera in p5
   //start detecting the tracking
   colors.on('track', function(event) { //this happens each time the tracking happens
       trackingData = event.data // break the trackingjs data into a global so we can access it with p5
   });
-
 }
 
 function draw() {
@@ -27,7 +28,13 @@ function draw() {
   //y varia entr e 0 cima e ~458 baixo
 
   // console.log(trackingData);
+  translate(capture.width, 0);
+  scale(-1, 1);
+  image(capture, 0, 0);
+
+
   total_side_movement = 0
+
   if(trackingData){ //if there is tracking data to look at, then...
     for (var i = 0; i < trackingData.length; i++) { //loop through each of the detected colors
       // console.log( trackingData[i] )
