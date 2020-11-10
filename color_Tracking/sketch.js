e71e85e8b805fc8cba6ce78de3d20e04aff450d5
diogoboinas <diogoboinas@gmail.com>
2020-11-10 20:20:28 +0000

    all the same :(

--- color_Tracking/sketch.js
+++ color_Tracking/sketch.js
@@ -8,35 +8,25 @@ function setup() {
   //capture.position(0,0) //move the capture to the top left
   capture.style('opacity',0.5)// use this to hide the capture later on (change to 0 to hide)...
   //capture.id("myVideo"); //give the capture an ID so we can use it in the tracker below.
-  capture.id("cap")
   capture.hide()
+  capture.id("cap")
   
-}
-
-function tracker(id ,colors) {
-  tracking.track(id, colors); // start the tracking of the colors above on the camera in p5
+  colors = new tracking.ColorTracker(['magenta']);
+  tracking.track("#cap", colors); // start the tracking of the colors above on the camera in p5
   //start detecting the tracking
   colors.on('track', function(event) { //this happens each time the tracking happens
       trackingData = event.data // break the trackingjs data into a global so we can access it with p5
   });
 }
 
+
 function draw() {
   //x varia entre 0 esquerda e ~618 direita
   //y varia entr e 0 cima e ~458 baixo
-
-  colors = new tracking.ColorTracker(['magenta']);
-
-  console.log(trackingData);
-  translate(capture.width, 0);
-  scale(-1, 1);
-  tracker("#cap",colors)
-  image(capture, 0, 0);
-
-  
-
   total_side_movement = 0
-
+  translate(capture.width, 0);
+  scale(-1, 1)
+  image(capture, 0 , 0);
   if(trackingData){ //if there is tracking data to look at, then...
     for (var i = 0; i < trackingData.length; i++) { //loop through each of the detected colors
       // console.log( trackingData[i] )
