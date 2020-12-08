b4a95ea21207ed2321286b526a2056f88bdb0ad5
pchurro <pedromchurro@gmail.com>
2020-12-08 01:43:34 +0000

    fix bug icons

--- color_Tracking/sketch.js
+++ color_Tracking/sketch.js
@@ -121,243 +121,247 @@ function generateAndDetect(){
       //console.log("Nothing")
     } else {
       event.data.forEach(function(rect) {
-        if(rect.x<xmoveatual){
-          if(rect.y<ymoveatual){
-            console.log("diagonal para a direita -subindo")
-            nummovs_d_c++;
-            if(nummovs_d_b>0){
-              nummovs_d_b--
-            }
-            if(nummovs_e_c>0){
-              nummovs_e_c--
-            }
-            if(nummovs_e_b>0){
-              nummovs_e_b--
-            }
-          }
-          if(rect.y>ymoveatual){
-            console.log("diagonal para a direita -descendo")
-            nummovs_d_b++
-            if(nummovs_d_c>0){
-              nummovs_d_c--
-            }
-            if(nummovs_e_c>0){
-              nummovs_e_c--
-            }
-            if(nummovs_e_b>0){
-              nummovs_e_b--
-            }
-          }
-          console.log("DIREITA")
-          nummovs_d++;
-          console.log(nummovs_d)
-          if(nummovs_d==5){
-            console.log("MOVIMENTO PARA A DIREITA COM SUCESSO")
-            nummovs_d=0;
+          if (interacting){
+              if(rect.x<xmoveatual){
+                  if(rect.y<ymoveatual){
+                      console.log("diagonal para a direita -subindo")
+                      nummovs_d_c++;
+                      if(nummovs_d_b>0){
+                          nummovs_d_b--
+                      }
+                      if(nummovs_e_c>0){
+                          nummovs_e_c--
+                      }
+                      if(nummovs_e_b>0){
+                          nummovs_e_b--
+                      }
+                  }
+                  if(rect.y>ymoveatual){
+                      console.log("diagonal para a direita -descendo")
+                      nummovs_d_b++
+                      if(nummovs_d_c>0){
+                          nummovs_d_c--
+                      }
+                      if(nummovs_e_c>0){
+                          nummovs_e_c--
+                      }
+                      if(nummovs_e_b>0){
+                          nummovs_e_b--
+                      }
+                  }
+                  console.log("DIREITA")
+                  nummovs_d++;
+                  console.log(nummovs_d)
+                  if(nummovs_d==5){
+                      console.log("MOVIMENTO PARA A DIREITA COM SUCESSO")
+                      nummovs_d=0;
 
-            if(movimentos_executar[movimento_atual]==moves[1]){
-              clear();
-              movimento_atual++;
-              img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
-            }
+                      if(movimentos_executar[movimento_atual]==moves[1]){
+                          clear();
+                          movimento_atual++;
+                          img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
+                      }
+                  }
+
+                  if(nummovs_e>0){
+                      nummovs_e--;
+                  }
+
+
+
+              }else if(rect.x >xmoveatual) {
+                  if(rect.y<ymoveatual){
+                      console.log("diagonal para a esquerda -subindo")
+                      nummovs_e_c++
+
+                      if(nummovs_d_c>0){
+                          nummovs_d_c--
+                      }
+                      if(nummovs_d_b>0){
+                          nummovs_d_b--
+                      }
+                      if(nummovs_e_b>0){
+                          nummovs_e_b--
+                      }
+                  }
+                  if(rect.y>ymoveatual){
+                      console.log("diagonal para a esquerda -descendo")
+                      nummovs_e_b++
+
+                      if(nummovs_d_c>0){
+                          nummovs_d_c--
+                      }
+                      if(nummovs_d_b>0){
+                          nummovs_d_b--
+                      }
+                      if(nummovs_e_c>0){
+                          nummovs_e_c--
+                      }
+
+                  }
+                  console.log("ESQUERDA")
+                  nummovs_e++;
+                  console.log(nummovs_e)
+                  if(nummovs_e==5){
+                      console.log("MOVIMENTO PARA A ESQUERDA COM SUCESSO")
+                      nummovs_e=0;
+
+                      if(movimentos_executar[movimento_atual]==moves[0]){
+                          clear();
+                          movimento_atual++;
+                          img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
+                      }
+                  }
+
+                  if(nummovs_d>0){
+                      nummovs_d--;
+                  }
+              }else if(rect.y<ymoveatual){
+                  if(rect.x<xmoveatual){
+                      console.log("diagonal para a direita -subindo")
+                      nummovs_d_c++
+                      if(nummovs_d_b>0){
+                          nummovs_d_b--
+                      }
+                      if(nummovs_e_c>0){
+                          nummovs_e_c--
+                      }
+                      if(nummovs_e_b>0){
+                          nummovs_e_b--
+                      }
+                  }
+                  if(rect.x>xmoveatual){
+                      console.log("diagonal para a esquerda -subindo")
+                      nummovs_e_c++
+                      if(nummovs_d_c>0){
+                          nummovs_d_c--
+                      }
+                      if(nummovs_e_c>0){
+                          nummovs_e_c--
+                      }
+                      if(nummovs_e_b>0){
+                          nummovs_e_b--
+                      }
+                  }
+                  console.log("CIMA")
+                  nummovs_c++;
+                  console.log(nummovs_c)
+                  if(nummovs_c==2){
+                      console.log("MOVIMENTO PARA CIMA COM SUCESSO")
+                      nummovs_c=0;
+                      if(movimentos_executar[movimento_atual]==moves[2]){
+                          clear();
+                          movimento_atual++;
+                          img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
+                      }
+                  }
+
+                  if(nummovs_b>0){
+                      nummovs_b--;
+                  }
+
+              }else if(rect.y >ymoveatual) {
+                  if(rect.x<xmoveatual){
+                      console.log("diagonal para a direita -descendo")
+                      nummovs_d_b++
+
+                      if(nummovs_d_c>0){
+                          nummovs_d_c--
+                      }
+                      if(nummovs_d_b>0){
+                          nummovs_d_b--
+                      }
+                      if(nummovs_e_b>0){
+                          nummovs_e_b--
+                      }
+                  }
+                  if(rect.x>xmoveatual){
+                      console.log("diagonal para a esquerda -descendo")
+                      nummovs_e_b++
+
+                      if(nummovs_d_c>0){
+                          nummovs_d_c--
+                      }
+                      if(nummovs_d_b>0){
+                          nummovs_d_b--
+                      }
+                      if(nummovs_e_c>0){
+                          nummovs_e_c--
+                      }
+
+                  }
+                  console.log("BAIXO")
+                  nummovs_b++;
+                  console.log(nummovs_b)
+                  if(nummovs_b=2){
+                      console.log("MOVIMENTO PARA BAIXO COM SUCESSO")
+                      nummovs_b=0;
+                      if(movimentos_executar[movimento_atual]==moves[3]){
+                          clear();
+                          movimento_atual++;
+                          img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
+                      }
+                  }
+
+                  if(nummovs_c>0){
+                      nummovs_c--;
+                  }
+              }
+              if(nummovs_d_c==3){
+                  console.log("MOVIMENTO PARA A Diagoal DIREITA Subir COM SUCESSO")
+                  nummovs_d_c=0
+                  if(movimentos_executar[movimento_atual]==moves[6]){
+                      clear();
+                      movimento_atual++;
+                      img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
+                  }
+              }
+              if(nummovs_d_b==3){
+                  console.log("MOVIMENTO PARA A Diagoal DIREITA Descer COM SUCESSO")
+                  nummovs_d_b=0
+                  if(movimentos_executar[movimento_atual]==moves[4]){
+                      clear();
+                      movimento_atual++;
+                      img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
+                  }
+              }
+              if(nummovs_e_c==3){
+                  console.log("MOVIMENTO PARA A Diagoal Esquerda Subir COM SUCESSO")
+                  nummovs_e_c=0
+                  if(movimentos_executar[movimento_atual]==moves[7]){
+                      clear();
+                      movimento_atual++;
+                      img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
+                  }
+              }
+              if(nummovs_e_b==3){
+                  console.log("MOVIMENTO PARA A Diagoal Esquerda Descer COM SUCESSO")
+                  nummovs_e_b=0
+                  if(movimentos_executar[movimento_atual]==moves[5]){
+                      clear();
+                      movimento_atual++;
+                      img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
+                  }
+              }
+
+              if(movimento_atual>=n_movimentos){
+                  clear();
+                  console.log("CONSEGUIU COMPLETAR O DESAFIO")
+                  // img = loadImage("../tick.png");
+                  sucessfulInteraction();
+              }
+              console.log("estamos na posiçao do array completo: "+movimento_atual)
+
+              xmoveatual=rect.x;
+              ymoveatual=rect.y;
+
+              atual_x=rect.x
+              atual_y=rect.y
           }
 
-          if(nummovs_e>0){
-            nummovs_e--;
-          }
-
-
-
-        }else if(rect.x >xmoveatual) {
-          if(rect.y<ymoveatual){
-            console.log("diagonal para a esquerda -subindo")
-            nummovs_e_c++
-
-            if(nummovs_d_c>0){
-              nummovs_d_c--
-            }
-            if(nummovs_d_b>0){
-              nummovs_d_b--
-            }
-            if(nummovs_e_b>0){
-              nummovs_e_b--
-            }
-          }
-          if(rect.y>ymoveatual){
-            console.log("diagonal para a esquerda -descendo")
-            nummovs_e_b++
-
-            if(nummovs_d_c>0){
-              nummovs_d_c--
-            }
-            if(nummovs_d_b>0){
-              nummovs_d_b--
-            }
-            if(nummovs_e_c>0){
-              nummovs_e_c--
-            }
-
-          }
-          console.log("ESQUERDA")
-          nummovs_e++;
-          console.log(nummovs_e)
-          if(nummovs_e==5){
-            console.log("MOVIMENTO PARA A ESQUERDA COM SUCESSO")
-            nummovs_e=0;
-
-            if(movimentos_executar[movimento_atual]==moves[0]){
-              clear();
-              movimento_atual++;
-              img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
-            }
-          }
-
-          if(nummovs_d>0){
-            nummovs_d--;
-          }
-        }else if(rect.y<ymoveatual){
-          if(rect.x<xmoveatual){
-            console.log("diagonal para a direita -subindo")
-            nummovs_d_c++
-            if(nummovs_d_b>0){
-              nummovs_d_b--
-            }
-            if(nummovs_e_c>0){
-              nummovs_e_c--
-            }
-            if(nummovs_e_b>0){
-              nummovs_e_b--
-            }
-          }
-          if(rect.x>xmoveatual){
-            console.log("diagonal para a esquerda -subindo")
-            nummovs_e_c++
-            if(nummovs_d_c>0){
-              nummovs_d_c--
-            }
-            if(nummovs_e_c>0){
-              nummovs_e_c--
-            }
-            if(nummovs_e_b>0){
-              nummovs_e_b--
-            }
-          }
-          console.log("CIMA")
-          nummovs_c++;
-          console.log(nummovs_c)
-          if(nummovs_c==2){
-            console.log("MOVIMENTO PARA CIMA COM SUCESSO")
-            nummovs_c=0;
-            if(movimentos_executar[movimento_atual]==moves[2]){
-              clear();
-              movimento_atual++;
-              img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
-            }
-          }
-
-          if(nummovs_b>0){
-            nummovs_b--;
-          }
-
-        }else if(rect.y >ymoveatual) {
-          if(rect.x<xmoveatual){
-            console.log("diagonal para a direita -descendo")
-            nummovs_d_b++
-
-            if(nummovs_d_c>0){
-              nummovs_d_c--
-            }
-            if(nummovs_d_b>0){
-              nummovs_d_b--
-            }
-            if(nummovs_e_b>0){
-              nummovs_e_b--
-            }
-          }
-          if(rect.x>xmoveatual){
-            console.log("diagonal para a esquerda -descendo")
-            nummovs_e_b++
-
-            if(nummovs_d_c>0){
-              nummovs_d_c--
-            }
-            if(nummovs_d_b>0){
-              nummovs_d_b--
-            }
-            if(nummovs_e_c>0){
-              nummovs_e_c--
-            }
-
-          }
-          console.log("BAIXO")
-          nummovs_b++;
-          console.log(nummovs_b)
-          if(nummovs_b=2){
-            console.log("MOVIMENTO PARA BAIXO COM SUCESSO")
-            nummovs_b=0;
-            if(movimentos_executar[movimento_atual]==moves[3]){
-              clear();
-              movimento_atual++;
-              img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
-            }
-          }
-
-          if(nummovs_c>0){
-            nummovs_c--;
-          }
-        }
-        if(nummovs_d_c==3){
-          console.log("MOVIMENTO PARA A Diagoal DIREITA Subir COM SUCESSO")
-          nummovs_d_c=0
-          if(movimentos_executar[movimento_atual]==moves[6]){
-            clear();
-            movimento_atual++;
-            img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
-          }
-        }
-        if(nummovs_d_b==3){
-          console.log("MOVIMENTO PARA A Diagoal DIREITA Descer COM SUCESSO")
-          nummovs_d_b=0
-          if(movimentos_executar[movimento_atual]==moves[4]){
-            clear();
-            movimento_atual++;
-            img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
-          }
-        }
-        if(nummovs_e_c==3){
-          console.log("MOVIMENTO PARA A Diagoal Esquerda Subir COM SUCESSO")
-          nummovs_e_c=0
-          if(movimentos_executar[movimento_atual]==moves[7]){
-            clear();
-            movimento_atual++;
-            img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
-          }
-        }
-        if(nummovs_e_b==3){
-          console.log("MOVIMENTO PARA A Diagoal Esquerda Descer COM SUCESSO")
-          nummovs_e_b=0
-          if(movimentos_executar[movimento_atual]==moves[5]){
-            clear();
-            movimento_atual++;
-            img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
-          }
-        }
-
-        if(movimento_atual>=n_movimentos){
-          clear();
-          console.log("CONSEGUIU COMPLETAR O DESAFIO")
-          // img = loadImage("../tick.png");
-          sucessfulInteraction();
-        }
-        console.log("estamos na posiçao do array completo: "+movimento_atual)
-
-        xmoveatual=rect.x;
-        ymoveatual=rect.y;
-
-        atual_x=rect.x
-        atual_y=rect.y
 
       });
+
     }
   });
 
@@ -377,6 +381,7 @@ function sucessfulInteraction(){
     movimentos_executar.push(moves[x])
   }
   movimento_atual=0;
+  img = loadImage("../arrows/"+movimentos_executar[movimento_atual]+".png");
 
   console.log(n_movimentos);
   console.log(movimentos_executar);
